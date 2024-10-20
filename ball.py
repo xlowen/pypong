d19d0218344d462b336850d25b813b86864f3c47
Amilton Siqueira <amiltonvsiqueira@gmail.com>
2024-10-20 19:28:35 -0300

    Create collision detections with paddles and walls

--- ball.py
+++ ball.py
@@ -2,6 +2,9 @@ from turtle import Turtle
 import random
 class Ball(Turtle):
 
+
+    heading_1 = 0;
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -11,8 +14,18 @@ class Ball(Turtle):
         self.speed(1)
 
     def ball_heading(self):
-        x = random.randint(-45, 45)
-        self.setheading(x)
+        self.heading_1 = random.randint(-65, 65)
+        print("random heading value = " + str(self.heading_1))
+        self.setheading(self.heading_1)
 
-    def ball_move(self):
+    def ball_move_fw(self):
         self.forward(15)
+
+    def ball_move_bw(self):
+        self.backward(15)
+    def bounce(self, heading):
+        print("this is the y angle: " + str(self.heading_1) + "this is the angle it should go: " + str(-self.heading_1))
+        self.setheading(-heading)
+
+    def bounce_back(self):
+        self.setheading(-self.heading_1)
--- main.py
+++ main.py
@@ -23,11 +23,36 @@ screen.onkey(paddle_2.move_down, "s")
 
 game_is_on = True
 ball.ball_heading()
+forward = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    ball.ball_move()
+
+    if forward:
+        ball.ball_move_fw()
+
+        if ball.distance(paddle_1) < 50 and ball.xcor() > 330:
+            print("ball.xcor() é:  " + str(ball.xcor()))
+            ball.ball_heading()
+            forward = False
+
+    else:
+        ball.ball_move_bw()
+
+        if ball.distance(paddle_2) < 50 and ball.xcor() < -330:
+            print("ball.xcor() é:  " + str(ball.xcor()))
+            ball.ball_heading()
+            forward = True
+
+    if ball.xcor() > 465 or ball.xcor() < -465:
+        print("outside at x", ball.xcor())
+        game_is_on = False
+
+    if ball.ycor() > 405 or ball.ycor() < -405:
+        print("outside at y", ball.ycor())
+        ball.bounce(ball.heading())
+
 
 # todo Create Screen
 # todo Create and move paddle
